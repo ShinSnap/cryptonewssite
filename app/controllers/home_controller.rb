b1f3abc7505e99a9a57509cd62709ccdd3ae240e
shinSnap <xshijimax@gmail.com>
2018-07-22 21:44:20 -0700

    Added price featuer w/ simple error checking

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,30 @@ class HomeController < ApplicationController
     require 'net/http'
     require 'json'
 
+    # Get News Data
     url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
     uri = URI(url)
     response = Net::HTTP.get(uri)
     @news = JSON.parse(response)
+
+    # Get Price Data
+    price_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETC,LTC,XRP,NEO&tsyms=USD'
+    price_uri = URI(price_url)
+    price_response = Net::HTTP.get(price_uri)
+    @prices = JSON.parse(price_response)
   end
 
   def prices
+    require 'net/http'
+    require 'json'
+
+    @symbol = params[:sym]
+    if @symbol
+      symbol_url = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=#{@symbol.upcase}&tsyms=USD"
+      symbol_uri = URI(symbol_url)
+      symbol_response = Net::HTTP.get(symbol_uri)
+      @symbol_price = JSON.parse(symbol_response)
+    end
+
   end
 end
